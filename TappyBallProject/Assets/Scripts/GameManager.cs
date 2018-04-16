371005e45af73fde8d5e3eb52d193c98e86ff69a
Mateusz Skrzat <mateusz.skrzat1996@gmail.com>
2018-04-16 19:33:11 +0200

    First commit of TappyBall full game

---
+++ TappyBallProject/Assets/Scripts/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager instance;
+	bool gameOver;
+
+	void Awake() {
+		DontDestroyOnLoad (this.gameObject);
+		if (instance == null) {
+			instance = this;
+		} 
+		else {
+			Destroy (this.gameObject);
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		gameOver = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void GameStart() {
+		GameObject.Find ("PipeSpawner").GetComponent<PipeSpawner> ().StartSpawningPipes ();
+		UIManager.instance.GameStart ();
+	}
+	public void GameOver() {
+		gameOver = false;
+		GameObject.Find ("PipeSpawner").GetComponent<PipeSpawner> ().StopSpawningPipes ();
+		ScoreManager.instance.StopScore ();
+		UIManager.instance.GameOver ();
+	}
+}
